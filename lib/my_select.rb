8262c22db54c984985d95a33589c8dcaefb6c309
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 17:37:14 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,20 +1,20 @@
-# def my_select(collection)
-# # your code here!
-# if collection.count == 0
-# 	return nil
-# else
-# 		counter = 0
-# 		new_array = []
-# 		while counter < collection.count
-# 			if collection[counter] % 2 == 0
-# 				yield collection[counter]
-# 				new_array << collection[counter]
-# 				counter += 1
-# 			else
-# 				puts collection[counter]
-# 				counter += 1
-# 			end
-# 		end
-# 		return new_array
-# 	end
-# end
+def my_select(collection)
+# your code here!
+if collection.count == 0
+	return nil
+else
+		counter = 0
+		new_array = []
+		while counter < collection.count
+			if collection[counter] % 2 == 0
+				yield collection[counter]
+				new_array << collection[counter]
+				counter += 1
+			else
+				puts collection[counter]
+				counter += 1
+			end
+		end
+		return new_array
+	end
+end
